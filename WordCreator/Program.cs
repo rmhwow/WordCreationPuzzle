660c94970bc5f2c40da5410b586a7853fb5dd66f
Morgan O'Leary <rmhwow@users.noreply.github.com>
2019-03-20 15:10:03 -0500

    Uploading files for WordCreator

---
+++ WordCreator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            String[] validWords = new String[] { "a", "bb", "ab", "bbb", "bab", "bbbb" };
+
+            WordBuilder wb = new WordBuilder(validWords);
+            wb.longestSet(wb.validWords);
+
+            String[] valid = new String[] { "c", "bb", "ab", "bbb", "bab", "bbbb" };
+
+            WordBuilder w = new WordBuilder(valid);
+            wb.longestSet(w.validWords);
+
+           
+        }
+      
+    }
+}
---
+++ WordCreator/WordBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCreator
+{
+    class WordBuilder
+    {
+        public String[] validWords;
+
+        public WordBuilder(String[] valid) {
+            this.validWords = valid;
+        }
+        /*Method to display the list to the user*/
+        public void printResults(Stack<String> valid)
+        {
+            if (valid == null)
+            {
+                throw new NullReferenceException("Sorry there are no valid words");
+            }
+            else
+            {
+                String options = null;
+
+                foreach (String s in valid)
+                {
+                    options += s + ", ";
+                }
+                String finalopt = options.Remove(options.LastIndexOf(','), 1);
+                Console.WriteLine("The longest valid set  is { " + finalopt + "}");
+            }
+        }
+
+        /*Method to determine the shortest word within the list.  I made the assumption that we have to first start with the shortest word on the list.  
+          This logic could change if we decided it needed to be the shortest word with the most potential words to build
+         */
+        public String shortestBuildWord(string[] words) {
+            String shortW = null;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (shortW == null)
+                {
+                    shortW = words[i];
+                }
+                else if (shortW.Length > words[i].Length)
+                {
+                    shortW = words[i];
+                }
+            }
+
+            return shortW;
+        }
+
+  
+        /* determines the longest set of legitimate words from the shortest word then calls the printResults method to display */
+        public void longestSet(string[] words)
+        {
+           
+            Stack<String> valid = new Stack<String>();
+            String shortW = shortestBuildWord(words);
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Contains(shortW) && words[i].Length >= 1)
+                {
+                    if (containsOnce(words[i], shortW))
+                    {
+                        valid.Push(words[i]);
+                    };
+                }
+            }
+
+
+           printResults(valid);
+
+        }
+        //Making the assumption that the shortest word shouldn't appear in a word more than once aka "bab" is okay but not "baba"
+        Boolean containsOnce(String s, String sub)
+        {
+            int i = s.IndexOf(sub);
+            return i != -1 && i == s.LastIndexOf(sub);
+        }
+    }
+}
